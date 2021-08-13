678f715899554c955878ca2400edd9a119315222
Andy Hsieh <34999291+shoukoo@users.noreply.github.com>
2021-08-13 21:40:31 +1000

    Add highlights for orgmode plugin (#2)
    Add highlights for orgmode plugin

--- lua/mei.lua
+++ lua/mei.lua
@@ -179,6 +179,11 @@ function mei.load_plugin_syntax()
 		LspDiagnosticsUnderlineInformation = { style = "undercurl", sp = mei.blue },
 		LspDiagnosticsUnderlineHint = { style = "undercurl", sp = mei.purple },
 
+		-- Orgmode
+		OrgAgendaDay = { fg = mei.light_pink },
+		OrgDONE = { fg = mei.green },
+		OrgTODO = { fg = mei.orange },
+
 		-- Telescope
 		TelescopeBorder = { fg = mei.white_pink },
 		TelescopePromptBorder = { fg = mei.pink },
