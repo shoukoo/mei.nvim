21d4a9983f21787166d379f47e6b48aab418f750
shoukoo <shoukoo.koike@gmail.com>
2021-09-08 21:56:38 +1000

    Change to different pink color

--- lua/mei.lua
+++ lua/mei.lua
@@ -24,15 +24,20 @@ local mei = {
 	-- mainly for pmenu
 	bg_alt = "#73797e",
 
-	pink = "#f73a84",
-	light_pink = "#f85396",
-	lighter_pink = "#f96ba5",
-	white_pink = "#fb9dc3",
+	pink2 = "#f73a84",
+	pink = "#fe56bd",
+	light_pink = "#f96ba5",
+	light_pink2 = "#fa85b4",
 	pink_white = "#fdcee1",
+	dark_pink = "#c64378",
 
-	green = "#b1d182",
+	keyword_color = "#fb9dc3", -- pink
+
+	green = "#b4fa85",
+	light_green = "#66ffcc",
 	brown = "#c78f70",
 	blue = "#7aa0c7",
+	light_blue = "#65a0f8",
 	dark_blue = "#34597e",
 	red = "#eb4034",
 	yellow = "#ffe841",
@@ -40,7 +45,7 @@ local mei = {
 	light_orange = "#ffe577",
 	orange = "#ff8d00",
 	purple = "#ba4ddb",
-	light_purple = "#e06ee0",
+	light_purple = "#e999e9",
 }
 
 function mei.highlight(group, color)
@@ -55,19 +60,19 @@ function mei.load_syntax()
 	local syntax = {
 		Normal = { fg = mei.fg, bg = mei.bg },
 
-		LineNr = { fg = mei.white_pink, bg = mei.bg_alt },
-		Visual = { fg = mei.bg, bg = mei.white_pink },
-		VisualNOS = { bg = mei.white_pink },
+		LineNr = { fg = mei.keyword_color, bg = mei.bg_alt },
+		Visual = { fg = mei.bg, bg = mei.keyword_color },
+		VisualNOS = { bg = mei.keyword_color },
 
-		String = { fg = mei.white_pink },
-		Number = { fg = mei.brown },
-		Float = { fg = mei.brown },
-		Boolean = { fg = mei.orange },
-		Constant = { fg = mei.orange },
+		String = { fg = mei.light_blue },
+		Number = { fg = mei.green },
+		Float = { fg = mei.green },
+		Boolean = { fg = mei.light_green },
+		Constant = { fg = mei.light_purple },
 		Character = { fg = mei.green },
 		NonText = { fg = mei.pink },
 
-		-- require() or pairs()
+		-- require() or pairs() or variable names
 		Identifier = { fg = mei.fg },
 		-- types int, long char
 		Type = { fg = mei.pink },
@@ -79,25 +84,25 @@ function mei.load_syntax()
 		PreCondit = { fg = mei.pink },
 		PreProc = { fg = mei.pink },
 		-- general if statement
-		Conditional = { fg = mei.pink },
+		Conditional = { fg = mei.keyword_color },
 		---- while loop
-		Repeat = { fg = mei.pink },
+		Repeat = { fg = mei.keyword_color },
 		-- case default
-		Label = { fg = mei.pink },
+		Label = { fg = mei.keyword_color },
 		-- other keywords
 		--
-		Keyword = { fg = mei.yellow },
+		Keyword = { fg = mei.keyword_color },
 		-- Search /<search keyword>
-		IncSearch = { fg = mei.bg, bg = mei.pink, style = mei.none },
+		IncSearch = { fg = mei.fg, bg = mei.pink, style = mei.none },
 		Search = { fg = mei.bg, bg = mei.pink },
 		-- comment
 		Comment = { fg = mei.dark_blue },
 		-- variables
-		Statement = { fg = mei.yellow },
+		Statement = { fg = mei.keyword_color },
 		-- function()
-		Function = { fg = mei.pink },
-		--Typedef = { fg = mei.red },
-		Operator = { fg = mei.light_orange },
+		Function = { fg = mei.keyword_color },
+		Typedef = { fg = mei.keyword_color },
+		Operator = { fg = mei.keyword_color },
 		-- try catch and throw
 		Exception = { fg = mei.pink },
 		Error = { fg = mei.red },
@@ -122,13 +127,13 @@ function mei.load_syntax()
 		EndOfBuffer = { fg = mei.bg, bg = mei.none },
 		-- popup menu
 		Pmenu = { fg = mei.fg, bg = mei.bg_popup },
-		PmenuSel = { fg = mei.bg_popup, bg = mei.white_pink },
+		PmenuSel = { fg = mei.bg_popup, bg = mei.keyword_color },
 		Directory = { fg = mei.orange, bg = mei.bg },
 		FoldColumn = { fg = mei.pink, bg = mei.bg_alt },
 		-- set colorcolumn=<num>
 		ColorColumn = { bg = mei.bg_alt },
 		-- Match parameter
-		MatchParen = { fg = mei.light_pink, bg = mei.bg_alt },
+		MatchParen = { fg = mei.pink, bg = mei.bg_alt },
 		Whitespace = { fg = mei.bg_alt },
 		Question = { fg = mei.yellow },
 		NormalFloat = { fg = mei.purple, bg = mei.bg_popup },
@@ -143,7 +148,7 @@ function mei.load_syntax()
 		SpellRare = { fg = mei.orange, bg = mei.none, style = "undercurl" },
 		QuickFixLine = { fg = mei.yellow, style = "bold" },
 
-		SpecialComment = { fg = mei.bg_alt },
+		SpecialComment = { fg = mei.pink },
 		Todo = { fg = mei.bg, bg = mei.dark_yellow },
 		Delimiter = { fg = mei.dark_yellow },
 		Ignore = { fg = mei.grey },
@@ -155,13 +160,13 @@ end
 function mei.load_plugin_syntax()
 	local plugin_syntax = {
 		-- Treesitter
-		TSFunction = { fg = mei.fg },
-		TSMethod = { fg = mei.fg },
-		TSKeywordFunction = { fg = mei.pink },
-		TSProperty = { fg = mei.fg },
-		TSType = { fg = mei.fg },
+		TSFunction = { fg = mei.pink },
+		TSMethod = { fg = mei.pink },
+		TSKeywordFunction = { fg = mei.keyword_color },
+		TSProperty = { fg = mei.yellow },
+		TSType = { fg = mei.yellow },
 		TSVariable = { fg = mei.fg },
-		TSPunctBracket = { fg = mei.light_pink },
+		TSPunctBracket = { fg = mei.fg },
 
 		-- LSP config
 		LspDiagnosticsSignError = { fg = mei.red },
@@ -179,8 +184,13 @@ function mei.load_plugin_syntax()
 		LspDiagnosticsUnderlineInformation = { style = "undercurl", sp = mei.blue },
 		LspDiagnosticsUnderlineHint = { style = "undercurl", sp = mei.purple },
 
+		-- Orgmode
+		OrgAgendaDay = { fg = mei.light_pink },
+		OrgDONE = { fg = mei.green },
+		OrgTODO = { fg = mei.orange },
+
 		-- Telescope
-		TelescopeBorder = { fg = mei.white_pink },
+		TelescopeBorder = { fg = mei.keyword_color },
 		TelescopePromptBorder = { fg = mei.pink },
 		TelescopeMatching = { fg = mei.pink },
 		TelescopeSelection = { fg = mei.yellow, bg = mei.bg_popup, style = "bold" },
